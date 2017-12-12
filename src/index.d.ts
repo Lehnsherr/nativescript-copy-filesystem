d7a7358f8a2f8fbafd1d960a0e2b3d5937713156
Joris Seyler <joris-alexander.seyler@scheer-group.com>
2017-12-12 14:13:25 +0100

    refactor index

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,12 +1,7 @@
 import { Common } from './copy-filesystem.common';
 export declare class CopyFilesystem extends Common {
-  // define your typings manually
-  // or..
-  // take the ios or android .d.ts files and copy/paste them here
 
-	copy(current_path: string, destionation_path: string, file_ending: any);
-  
-    hasPermission_external_storage();
-    
-    log_FolderEntities(log_FolderEntities: string)
+	copy(current_path: string, destionation_path: string, file_ending: any): void;
+    hasPermission_external_storage(): void;
+    log_FolderEntities(log_FolderEntities: string): void;
 }
