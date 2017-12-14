42f0268c08bf65b07e9451e6ee551a24c756f057
Joris Seyler <joris-alexander.seyler@scheer-group.com>
2017-12-14 08:50:48 +0100

    let - var -.-

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -20,16 +20,16 @@ export class HelloWorldModel extends Observable {
 		if (!application.ios) {
 			this.copyFilesystem.hasPermission_external_storage()
 
-			var downloads_path = fs.path.join(android.os.Environment.getExternalStoragePublicDirectory(android.os.Environment.DIRECTORY_DOWNLOADS).toString());
-			var dcim_path = fs.path.join(android.os.Environment.getExternalStoragePublicDirectory(android.os.Environment.DIRECTORY_DCIM).toString());
+			let downloads_path = fs.path.join(android.os.Environment.getExternalStoragePublicDirectory(android.os.Environment.DIRECTORY_DOWNLOADS).toString());
+			let dcim_path = fs.path.join(android.os.Environment.getExternalStoragePublicDirectory(android.os.Environment.DIRECTORY_DCIM).toString());
 			// var ringtones_path = fs.path.join(android.os.Environment.getExternalStoragePublicDirectory(android.os.Environment.DIRECTORY_RINGTONES).toString());
 			// var movie_path = fs.path.join(android.os.Environment.getExternalStoragePublicDirectory(android.os.Environment.DIRECTORY_MOVIES).toString());
 
-			var folder = fs.Folder.fromPath(downloads_path);
+			let folder = fs.Folder.fromPath(downloads_path);
 
-			var test_txt = folder.getFile('test.txt');
-			var test_svg = folder.getFile('typescript_img.svg');
-			var test_png = folder.getFile('img_80.png');
+			let test_txt = folder.getFile('test.txt');
+			let test_svg = folder.getFile('typescript_img.svg');
+			let test_png = folder.getFile('img_80.png');
 
 
 			// Test file Copy
@@ -41,8 +41,8 @@ export class HelloWorldModel extends Observable {
 			this.copyFilesystem.copy(downloads_path, dcim_path, 'copy')
 			// this.copyFilesystem.log_FolderEntities(dcim_path)
 
-			var dcim = dcim_path + '/Download_copy(2)'
-			var folder_dcim = fs.Folder.fromPath(dcim);
+			let dcim = dcim_path + '/Download_copy(2)'
+			let folder_dcim = fs.Folder.fromPath(dcim);
 
 			console.log('Folder to delete: ' + folder_dcim.path + folder_dcim.name)
 
@@ -58,28 +58,28 @@ export class HelloWorldModel extends Observable {
 		}
 
 		if (application.ios) {
-			var doc_folder = fs.knownFolders.documents();
+			let doc_folder = fs.knownFolders.documents();
 
 			console.log(doc_folder.path, doc_folder.name)
 
-			var doc_parent = doc_folder.parent
+			let doc_parent = doc_folder.parent
 			console.log(doc_parent.path, doc_parent.name)
 
 
-			var doc_parent_parent = doc_parent.parent
+			let doc_parent_parent = doc_parent.parent
 			console.log(doc_parent_parent.path, doc_parent_parent.name)
 
 
 			this.copyFilesystem.log_FolderEntities(doc_folder.path)
 			
-			var path = fs.path.join(doc_folder.path, "test.txt");
-			var file = fs.File.fromPath(path);
+			let path = fs.path.join(doc_folder.path, "test.txt");
+			let file = fs.File.fromPath(path);
 
 			console.log('test.txt| path: ' + path, ' file.path: ' + file.path, ' file.name: ' + file.name)
 
-			var test_txt = folder.getFile('test.txt');
-			var test_svg = folder.getFile('typescript_img.svg');
-			var test_png = folder.getFile('img_80.png');
+			let test_txt = doc_folder.getFile('test.txt');
+			let test_svg = doc_folder.getFile('typescript_img.svg');
+			let test_png = doc_folder.getFile('img_80.png');
 
 
 			// Test file Copy
